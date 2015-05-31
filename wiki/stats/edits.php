91f8ee97e7bba8a321c1b80759bfd1986f6f8641
Rachel Fish <itsrachelfish@gmail.com>
2015-05-31 00:09:10 -0600

    Files for generating activity graphs (wiki.wetfish.net/daily)

---
+++ wiki/stats/edits.php
@@ -0,0 +1,29 @@
+<?php
+
+require('../functions.php');
+require('phpgraphlib.php'); 
+
+date_default_timezone_set('America/New_York');
+
+// Use date to generate the current day.
+// Omitting the specific time passed makes strtotime() return today's first second.
+
+$Today = strtotime(date("d F Y"));
+$Day = 86400; // Seconds
+
+$LastWeek = $Today - ($Day * 30);
+
+$Query = mysql_query("Select `EditTime` from `Wiki_Edits` where `EditTime` > $LastWeek");
+while(list($Time) = mysql_fetch_array($Query))
+{
+	$When = date('d M', $Time);
+	$Data[$When]++;
+}
+
+$Graph = new PHPGraphLib(800,400);
+$Graph->addData($Data);
+$Graph->setTitle("Edits This Month");
+$Graph->setTextColor("blue");
+$Graph->createGraph();
+
+?>
