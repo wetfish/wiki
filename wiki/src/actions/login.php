fc5049f79620c4dcc8d7ff2adfb14927281fe0af
Rachel Fish <itsrachelfish@gmail.com>
2015-06-23 17:31:01 -0600

    Created special login option for admins

--- wiki/src/actions/login.php
+++ wiki/src/actions/login.php
@@ -13,10 +13,28 @@ function login($path, $action, $title, $content)
             $content['Body'] = "YOU DID IT FRIEND!!<br /><br />You will now be brought back to your previous page.";
             $content['Body'] .= Redirect(str_replace("//", "/", "/$path"));
         }
+
+        elseif($_POST['Password'] == ADMIN_PASSWORD)
+        {
+            $_SESSION['bypass'] = true;
+            $_SESSION['admin'] = true;
+            $content['Body'] = "Wow, you're an admin!!<br /><br />You will now be brought back to your previous page.";
+            $content['Body'] .= Redirect(str_replace("//", "/", "/$path"));
+        }
+
+        else
+        {
+            $content['Body'] = "nope";
+        }
     }
-    
-    if(empty($_SESSION['bypass']))
+
+    else
     {
+        if(!empty($_SESSION['bypass']))
+        {
+            $content['Body'] .= "<b>Hey, you're already logged in!</b><br>";
+        }
+
         $content['Body'] .= "Protip: The super secret password is the same as the PIBDGAF ichc password.";
         
         $Form['_Options'] = "action:".str_replace("//", "/", "/$path/?login").";";
@@ -26,6 +44,6 @@ function login($path, $action, $title, $content)
         
         $content['Body'] .= Format($Form, Form);
     }
-
+    
     return array($title, $content);
 }
--- wiki/src/config.example.php
+++ wiki/src/config.example.php
@@ -10,6 +10,9 @@ define("MYSQL_DATABASE", "database");
 // Password used to avoid captchas via the login page
 define("LOGIN_PASSWORD", "password"); 
 
+// Password for admins to do admin things via the login page
+define("ADMIN_PASSWORD", "password");
+
 // Password used to ban users
 define("BAN_PASSWORD", "password");
 
