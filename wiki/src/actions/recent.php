0d5d9e80db4c1c83e8dc1d92ed372e9df3f1a0ef
Rachel Fish <itsrachelfish@gmail.com>
2015-06-23 18:58:00 -0600

    Moved recent and source to separate files, updated to hide archived edits

---
+++ wiki/src/actions/recent.php
@@ -0,0 +1,115 @@
+<?php
+
+function recent($path, $action, $title, $content)
+{
+//		$Head = '<meta name="robots" content="noindex, nofollow" />';
+    $content['UserNav']->Active("Recent Activity");
+
+    if(empty($_SESSION['Recent']))
+    {
+        $_SESSION['Recent']['Active'][0] = "All Edits";
+        $_SESSION['Recent']['Active'][3] = "Descending";
+        $_SESSION['Recent']['Order'] = "DESC";
+    }
+
+    $content['ExtraNav'] = new Navigation;
+    $content['ExtraNav']->Add("All Edits", "?recent/all");
+    $content['ExtraNav']->Add("Most Recent Edit", "?recent/unique");
+    $content['ExtraNav']->Add("Ascending", "?recent/asc");
+    $content['ExtraNav']->Add("Descending", "?recent/desc");
+    $content['ExtraNav']->Active($_SESSION['Recent']['Active']);
+
+    $Template['Title'] = "View:";
+    $content['ExtraNav']->Template($Template);
+
+    if(empty($action[1]))
+    {
+        $content['Title'] = "Recent Activity";
+
+        $ActivityQuery = "SELECT $Unique `ID`,`PageID`,`AccountID`,`EditTime`,`Size`,`Tags`,`Name`,`Description`,`Title` FROM `Wiki_Edits` where `Archived` = 0 ORDER BY `ID` {$_SESSION['Recent']['Order']}";
+        list($QueryData, $Links) = Paginate($ActivityQuery, 50, $_GET['page'], $_SERVER['QUERY_STRING']);
+        
+        $content['Body'] .= "<center class='page-navigation'>$Links</center>";
+        $content['Body'] .= "<table width='100%' class='history'>";
+        $content['Body'] .= "<tr><td><b>Revision</b></td><td><b>Size</b></td><td><b>Tags</b></td><td><b>Editor</b></td><td style='min-width:200px;'><b>Title</b></td><td><b>Description</b></td></tr>";
+        
+        if($QueryData)
+        {
+            foreach($QueryData as $Result)
+            {
+                list($EditID, $PageID, $AccountID, $PageTime, $PageSize, $pageTags, $PageName, $PageDescription, $PageTitle) = $Result;
+
+                if(empty($Data[$PageID]))
+                {
+                    $PageQuery = mysql_query("SELECT `Path` FROM `Wiki_Pages` WHERE `ID`='$PageID'");
+                    list($PagePath) = mysql_fetch_array($PageQuery);
+
+                    $Data[$PageID] = $PagePath;
+                }
+                else
+                    $PagePath = $Data[$PageID];
+
+                $Toggle++;
+                
+                date_default_timezone_set('America/New_York');
+                $minWidth = (recentTime($PageTime)) ? 85 : 175;
+                $PageTime = formatTime($PageTime);
+
+
+                if($Toggle % 2 == 1)
+                    $Class = "class='toggle'";
+                else
+                    $Class = '';
+
+                $PageName = FishFormat($PageName, "format");
+                $PageDescription = FishFormat($PageDescription, "format");
+                $PageTitle = FishFormat($PageTitle, "format");
+                $DiffURL = str_replace("//", "/", "/$PagePath/?diff/$EditID");
+                
+                $content['Body'] .= "<tr $Class><td style='min-width:{$minWidth}px;'>$PageTime</td><td>$PageSize</td><td>$pageTags</td><td><b><a href='/names?id=$AccountID'>$PageName</a></b></td><td style='max-width:400px'><span style='float:right;'><a href='$DiffURL' rel='nofollow'>d</a></span><b><a href='/$PagePath'>$PageTitle</a></b></td><td class='multi-line'>$PageDescription</td></tr>";
+            }
+        }
+
+        $content['Body'] .= "</table>";
+        $content['Body'] .= "<center class='page-navigation bottom'>$Links</center>";			
+    }
+    elseif($action[1] == "page")
+    {
+
+    }
+    else
+    {
+        switch($action[1])
+        {
+            case "all":
+                $_SESSION['Recent']['Active'][0] = "All Edits";
+                $_SESSION['Recent']['Active'][1] = "";
+                $_SESSION['Recent']['View'] = "";
+            break;
+
+            case "unique":
+                $_SESSION['Recent']['Active'][0] = "";
+                $_SESSION['Recent']['Active'][1] = "Most Recent Edit";
+                $_SESSION['Recent']['View'] = "DISTINCT";
+            break;
+
+            case "asc":
+                $_SESSION['Recent']['Active'][2] = "Ascending";
+                $_SESSION['Recent']['Active'][3] = "";
+                $_SESSION['Recent']['Order'] = "";
+            break;
+
+            case "desc":
+                $_SESSION['Recent']['Active'][2] = "";
+                $_SESSION['Recent']['Active'][3] = "Descending";
+                $_SESSION['Recent']['Order'] = "DESC";
+            break;
+        }
+
+        $content['Body'] = "<b>Settings changed...</b> <meta http-equiv='refresh' content='2;url=".FormatPath("/$path/?recent")."'>";
+    }
+
+    return array($title, $content);
+}
+
+?>
---
+++ wiki/src/actions/source.php
@@ -0,0 +1,39 @@
+<?php
+
+function source($path, $action, $title, $content)
+{
+    $Head = '<meta name="robots" content="noindex, nofollow" />';
+    $content['PageNav']->Active("Page History");
+
+    if(is_numeric($action[1]))
+    {
+        $PageQuery = mysql_query("SELECT `AccountID`,`EditTime`,`Name`,`Description`,`Title`,`Content` FROM `Wiki_Edits` WHERE `ID`='$action[1]' and `Archived` = 0");
+        list($AccountID, $PageEditTime, $PageName, $PageDescription, $PageTitle, $PageContent) = mysql_fetch_array($PageQuery);
+
+        $Form['_Options'] = "action:;";
+
+        $Form['Name']['Text'] = "Name:";
+        $Form['Name']['Form'] = "id:Name; name:Name; value:{".$PageName."}; maxlength:32;";
+
+        $Form['Title']['Text'] = "Title:";
+        $Form['Title']['Form'] = "name:Title; value:x{".$PageTitle."}x; maxlength:255;";
+
+        $Form['Content']['Text'] = "Content:";
+        $Form['Content']['Form'] = "name:Content; value:x{".$PageContent."}x; type:textarea; cols:80; rows:12;";
+
+        $Form['Description']['Text'] = "Description:";
+        $Form['Description']['Form'] = "name:Description; value:x{".$PageDescription."}x; size: 80; maxlength:255;";
+
+        $content['Title'] = "Viewing Source of: $PageTitle";
+        $content['Body'] .= Format($Form, Form);
+
+        date_default_timezone_set('America/New_York');
+//			$PageEditTime = date("F j\, Y G:i:s", $PageEditTime)." EST";
+        $PageEditTime = formatTime($PageEditTime);
+        $content['Footer'] = "This page is an old revision made by <b><a href='/names?id=$AccountID'>$PageName</a></b> on $PageEditTime.";
+    }
+
+    return array($title, $content);
+}
+
+?>
