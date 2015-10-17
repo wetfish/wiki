aeb74ac94d624fb1c6d53cbf923678c87031a2e1
Rachel Fish <itsrachelfish@gmail.com>
2015-10-16 23:54:28 -0600

    Updated wiki API to allow logging in

--- wiki/api/v1/api.php
+++ wiki/api/v1/api.php
@@ -4,12 +4,16 @@
 require "../../functions.php";
 require "../../src/markup/fishformat.php";
 
+// Recaptcha library for logging in
+require "../../recaptchalib.php";
+
 class API
 {
     // The model must be passed when the API is constructed
     function __construct($model)
     {
         $this->model = $model;
+        session_start();
     }
 
     // Function to process requests
@@ -108,6 +112,38 @@ class API
             header('Content-Type: application/json');
             return json_encode($response);
         }
+    }
+
+    public function login()
+    {
+        // Check if post data was submitted
+        if(!empty($_POST))
+        {
+            if(preg_match("/^(fuck? you captcha?|fuck? captchas?|i hate captchas?|captchas?.*sucks?)$/i", $_POST["recaptcha_response_field"]))
+                $_SESSION['bypass'] = true;
+
+            if(!isset($_SESSION['bypass']))
+            {
+                $Resp = recaptcha_check_answer(RECAPTCHA_PRIVATE,
+                                    $_SERVER["REMOTE_ADDR"],
+                                    $_POST["recaptcha_challenge_field"],
+                                    $_POST["recaptcha_response_field"]);
+
+                if(!$Resp->is_valid)
+                    return json_encode(array('status' => 'error', 'message' => 'Invalid captcha! Please try again.'));
+                else
+                    $_SESSION['bypass'] = true;
+            }
+        }
+
+        // Check if the user is now logged in
+        if($_SESSION['bypass'])
+            return json_encode(array('status' => 'success', 'message' => 'You are logged in.'));
+
+        // Otherwise return a captcha
+        $script = "<script>var RecaptchaOptions = {theme : 'blackglass'}</script>";
+        $form = "<form method='post'>" . recaptcha_get_html(RECAPTCHA_PUBLIC, null, 1) . "</form>";
+        return $script . $form;
     }
 }
 
