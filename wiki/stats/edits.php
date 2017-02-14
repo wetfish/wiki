eb5a08407cf792d3063b5c261094998cb025b77d
Rachel Fish <itsrachelfish@gmail.com>
2017-02-14 01:38:08 -0700

    Different image size and date formatting options for year vs month

--- wiki/stats/edits.php
+++ wiki/stats/edits.php
@@ -11,22 +11,45 @@ if($mode == "year")
 {
     $title = "Edits This Year";
     $display = "365 days ago";
+    $width = 1800;
+    $height = 768;
 }
 else
 {
     $title = "Edits This Month"; 
     $display = "30 days ago";
+    $width = 1024;
+    $height = 600;
 }
 
 $display = strtotime($display);
 $query = mysql_query("Select `EditTime` from `Wiki_Edits` where `EditTime` > {$display} order by `EditTime` asc");
+
 while(list($time) = mysql_fetch_array($query))
 {
-    $when = date('d M', $time);
+    if($mode == "year")
+    {
+        $when = date('M n/d', $time);
+        $month = date('M', $time);
+
+        if(!isset($months[$month]))
+        {
+            $months[$month] = true;
+        }
+        else
+        {
+            $when = date('n/d', $time);
+        }
+    }
+    else
+    {
+        $when = date('M d', $time);
+    }
+
     $data[$when]++;
 }
 
-$graph = new PHPGraphLib(1800,768);
+$graph = new PHPGraphLib($width, $height);
 $graph->addData($data);
 $graph->setTitle($title);
 $graph->setTextColor("blue");
