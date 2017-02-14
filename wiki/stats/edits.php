b600f8e999917c85c4908a2243ac1d92b336e6c7
Rachel Fish <itsrachelfish@gmail.com>
2017-02-14 01:18:25 -0700

    Added super bad looking year mode

--- wiki/stats/edits.php
+++ wiki/stats/edits.php
@@ -5,17 +5,30 @@ require('phpgraphlib.php');
 
 date_default_timezone_set('America/New_York');
 
-$display = strtotime("30 days ago");
-$query = mysql_query("Select `EditTime` from `Wiki_Edits` where `EditTime` > $display order by `EditTime` asc");
+$mode = $_GET['mode'];
+
+if($mode == "year")
+{
+    $title = "Edits This Year";
+    $display = "365 days ago";
+}
+else
+{
+    $title = "Edits This Month"; 
+    $display = "30 days ago";
+}
+
+$display = strtotime($display);
+$query = mysql_query("Select `EditTime` from `Wiki_Edits` where `EditTime` > {$display} order by `EditTime` asc");
 while(list($time) = mysql_fetch_array($query))
 {
     $when = date('d M', $time);
     $data[$when]++;
 }
 
-$graph = new PHPGraphLib(800,400);
+$graph = new PHPGraphLib(1800,768);
 $graph->addData($data);
-$graph->setTitle("Edits This Month");
+$graph->setTitle($title);
 $graph->setTextColor("blue");
 $graph->createGraph();
 
