0dced0aab65a9834a3fd668b5d66b46386187e16
Rachel Fish <itsrachelfish@gmail.com>
2021-03-13 16:36:34 -0700

    Defined a new env variable for allowed embed domains

--- wiki/load.php
+++ wiki/load.php
@@ -1,24 +1,9 @@
 <?php
 
 $URL = parse_url($_GET['url']);
-require('simple_html_dom.php');
-require('cssparser.php');
+$allowed = getenv('ALLOWED_EMBEDS');
 
-function uuid($prefix = '')
-{
-    $chars = md5(uniqid(mt_rand(), true));
-    $uuid  = substr($chars,0,8) . '-';
-    $uuid .= substr($chars,8,4) . '-';
-    $uuid .= substr($chars,12,4) . '-';
-    $uuid .= substr($chars,16,4) . '-';
-    $uuid .= substr($chars,20,12);
-    return $prefix . $uuid;
-}
-
-$site_url = getenv('SITE_URL');
-$match = '/' . preg_quote($site_url) . '$/i';
-
-if(preg_match($match, $URL['host']))
+if(preg_match($allowed, $URL['host']))
 {
     echo file_get_contents($_GET['url']);
 }
