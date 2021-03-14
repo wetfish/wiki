19c7e7e08c2eff1058f438f82a3641e738dd64f9
Michael Misch <michaelmisch1985@gmail.com>
2021-03-13 20:59:21 -0800

    Upload fix (#46)
    * Start working on uploads
    
    * Fix formatting
    
    * The semicolon
    
    * Start working on uploads
    
    * Fix formatting
    
    * Add in proper libjpeg path, add in note about upload/thumbs
    
    * Make sure we return on probable malicous upload

--- wiki/upload.php
+++ wiki/upload.php
@@ -24,42 +24,76 @@ if($_FILES)
     }
     else
     {
-        $ImageInfo = pathinfo($Image['name']);
+        $Mime = mime_content_type($Image['tmp_name']);
+        switch($Mime){
+            case "image/jpg":
+                $Extension = "jpg";
+                break;
+            case "image/jpeg":
+                $Extension = "jpeg";
+                break;
+            case "image/gif":
+                $Extension = "gif";
+                break;
+            case "image/png":
+                $Extension = "png";
+                break;
+            case "audio/mp3":
+                $Extension = "mp3";
+                break;
+            case "audio/webm":
+            case "video/webm":
+                $Extension = "webm";
+                break;
+            case "video/mp4":
+                $Extension = "mp4";
+                break;
+            case "audio/midi":
+            case "audio/xmidi":
+                $Extension = "mid";
+                break;
+            case "image/svg+xml":
+                $Extension = "svg";
+                break;
+            default:
+            // Catch all for the various text types that may end up being parsed, accept them as text files
+                if(preg_match('/text\/.*/i', $Mime))
+                    $Extension = "txt";
+                else {
+                    echo "HACKER!!!!!!!!!!!!!";
+                    return;
+		}
+        }
+    
         $Filename = uuid();
-        $Extension = $ImageInfo['extension'];
-
-        if(preg_match('/^(jpe?g|gif|png|txt|mp3|webm|mp4|mid|svg)$/i', $Extension))
-        {
-            while(file_exists("upload/$Filename.$Extension"))
+        while(file_exists("upload/$Filename.$Extension"))
             {
                 $Filename = uuid();
             }
 
-            move_uploaded_file($Image['tmp_name'], "upload/$Filename.$Extension");
-            chmod("upload/$Filename.$Extension", 0644);
+        move_uploaded_file($Image['tmp_name'], "upload/$Filename.$Extension");
+        chmod("upload/$Filename.$Extension", 0644);
 
-            $Time = time();
+        $Time = time();
 
-            if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-                $userIP = $_SERVER['HTTP_X_FORWARDED_FOR'];
-            else
-                $userIP = $_SERVER['REMOTE_ADDR'];
-
-            // Make sure the user IP is sanitized
-            $userIP = preg_replace('/[^0-9.]/', '', $userIP);
-
-            mysql_query("Insert into `Images` values ('NULL', '$Time', '', '$userIP', '{$Image['name']}', 'upload/$Filename.$Extension')");
-
-            if(!empty($_GET['api']))
-            {
-                header("Location: /upload/{$Filename}.{$Extension}");
-                exit;
-            }
-
-            echo "<meta http-equiv='refresh' content='2;url=upload/$Filename.$Extension'>Image added!";
-        }
+        if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+            $userIP = $_SERVER['HTTP_X_FORWARDED_FOR'];
         else
-            echo "HACKER!!!!!!!!!!!!!!";
+            $userIP = $_SERVER['REMOTE_ADDR'];
+
+        // Make sure the user IP is sanitized
+        $userIP = preg_replace('/[^0-9.]/', '', $userIP);
+
+        mysql_query("Insert into `Images` values ('NULL', '$Time', '', '$userIP', '{$Image['name']}', 'upload/$Filename.$Extension')");
+
+        if(!empty($_GET['api']))
+        {
+            header("Location: /upload/{$Filename}.{$Extension}");
+            exit;
+        }
+
+        $Mime = preg_replace('/[^a-zA-Z0-9].*/', '', $Mime);
+        echo "<meta http-equiv='refresh' content='2;url=upload/$Filename.$Extension'>$Mime added!";
     }
 }
 else
