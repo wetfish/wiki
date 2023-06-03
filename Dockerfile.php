fd4508d4a40229119abef49ebec9d695d8c06b95
Edwin Pers <pers.edwin@gmail.com>
2023-06-03 20:49:56 +0000

    rework for newer debian base image, other fixes - rewrite dockerfiles to suit - change how we pass in php-fpm configuration - update workflows - update/normalize example config - improve readme a little bit - add commented out bind mounts for wwwroot in dev envs

--- Dockerfile.php
+++ Dockerfile.php
@@ -3,103 +3,75 @@
 #
 # Small note, make sure to create upload/thumbs prior to running this! 
 
-# builder container
-FROM docker.io/debian:stretch AS npm
+#
+# runtime container
+FROM docker.io/debian:bookworm-slim
 
 # install some packages
 RUN set -exu \
   && DEBIAN_FRONTEND=noninteractive apt-get -yq update \
   && DEBIAN_FRONTEND=noninteractive apt-get -yq install \
-    curl \
-    apt-utils \
-    git \
     libjpeg-dev \
     libpng-dev \
-    libfreetype6-dev
+    libfreetype6-dev \
+    software-properties-common \
+    ca-certificates \
+    lsb-release \
+    apt-transport-https \
+    curl
 
 # setup nodejs repo
-RUN curl -fsSL https://deb.nodesource.com/setup_14.x | bash -
+RUN set -exu \
+  && curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | gpg --dearmor | apt-key add - \
+  && echo "deb https://deb.nodesource.com/node_14.x bookworm main" | tee /etc/apt/sources.list.d/nodesource.list
 
+# acquire install-php-extensions script
+RUN set -exu \
+  && curl -fsSL https://github.com/mlocati/docker-php-extension-installer/releases/download/2.1.28/install-php-extensions \
+    -o /usr/local/bin/install-php-extensions \
+  && chmod +x /usr/local/bin/install-php-extensions
+
+# setup php5.6 repo
+RUN set -exu \
+  && echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list \
+  && curl -fsSL https://packages.sury.org/php/apt.gpg | apt-key add -
+
+# install php5.6, some extensions, and nodejs
 RUN set -exu \
   && DEBIAN_FRONTEND=noninteractive apt-get -yq update \
   && DEBIAN_FRONTEND=noninteractive apt-get -yq install \
-    nodejs
+    php5.6 \
+    php5.6-fpm \
+    php5.6-mysqli \
+    php5.6-mysql \
+    php5.6-exif \
+    php5.6-gd \
+    nodejs \
+    npm
 
+# clean apt caches
 RUN set -exu \
   && DEBIAN_FRONTEND=noninteractive apt-get -yq clean
 
-# copy in wwwroot
+# copy in sources
 COPY ./wwwroot /var/www
 
-# run npm install
+# make sure our user owns the wwwroot
 RUN set -exu \
-  && npm install --silent --prefix /var/www/src
-
-
-#
-# runtime container
-FROM docker.io/php:5.6-fpm-stretch
-
-# install some packages
-RUN set -exu \
-  && DEBIAN_FRONTEND=noninteractive apt-get -yq update \
-  && DEBIAN_FRONTEND=noninteractive apt-get -yq install \
-    libjpeg-dev \
-    libpng-dev \
-    libfreetype6-dev
-
-# copy populated wwwroot from build container
-COPY --from=npm /var/www /var/www
-
-# create our user
-RUN set -exu \
-  && addgroup --gid 1101 fishy \
-  && adduser \
-      --uid 1101 \
-      --ingroup fishy \
-      --no-create-home \
-      --shell /sbin/nologin \
-      --disabled-password \
-      fishy \
-  && chown -R fishy:fishy /var/www
-
-# setup nodejs repo
-# RUN curl -sL https://deb.nodesource.com/setup_14.x | bash -
-
-# install some packages
-# RUN set -exu \
-#   && DEBIAN_FRONTEND=noninteractive apt-get -yq update \
-#   && DEBIAN_FRONTEND=noninteractive apt-get -yq install \
-#     apt-utils \
-#     git \
-#     libjpeg-dev \
-#     libpng-dev \
-#     libfreetype6-dev \
-#     nodejs \
-#   && DEBIAN_FRONTEND=noninteractive apt-get -yq clean
-
-# configure some php stuff
-RUN set -exu \
-  && docker-php-ext-configure gd \
-    --with-freetype-dir=/usr/include/freetype2/ \
-    --with-jpeg-dir=/usr/include/libjpeg \ 
-  && docker-php-ext-install \
-    mysqli \
-    mysql \
-    exif \
-    gd
-
-# run npm install
-# RUN set -exu \
-#   && mkdir -p /var/www/src/node_modules \
-#   && npm install --prefix /var/www/src
+  && chown -R www-data:www-data /var/www
 
 # switch to our nonroot user
-USER fishy
+USER www-data
 
-# Expose port 9000 and start php-fpm server
+# run npm install
+RUN set -exu \
+  && npm install --prefix /var/www/src
+
+# back to root
+USER root
+
 WORKDIR /var/www
 
+# Expose port 9000 and start php-fpm server
 EXPOSE 9000
-
-CMD ["php-fpm"]
+CMD ["/usr/sbin/php-fpm5.6", "--nodaemonize", "--force-stderr"]
--- README.md
+++ README.md
@@ -9,6 +9,11 @@ cp mariadb.env.example mariadb.env
 # -> edit, change passwords and other info as needed
 cp php.env.example php.env
 # -> edit, change passwords to match
+cp config/ban.php.example config/ban.php
+# -> add ipbans
+cp config/config.php.example config/config.php
+# -> admin password
+
 
 docker compose \
   -f docker-compose.dev.yml \
@@ -21,10 +26,13 @@ docker compose -f docker-compose.dev.yml logs -f
 
 The service will be available at [http://127.0.0.1:2405](http://127.0.0.1:2405)
 
+If you'd like, /etc/hosts wiki.wetfish.net.local to 127.0.0.1 and browse to that.
+
 ## When do I need to rebuild the container?
 
 Whenever you make an edit in wwwroot. \
-If you're brave, you could bind mount in a devlopment directory to `/var/www`
+If you're brave, you could edit docker-compose.dev.yml and uncomment the bind mount for wwwroot \
+If you do that, you're responsible for running `npm install` !
 
 ## To get search, tags, etc to work
 
