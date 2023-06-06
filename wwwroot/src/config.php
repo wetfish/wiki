57845892f71fdc23788035f89d17db550abcb2db
Edwin Pers <pers.edwin@gmail.com>
2023-06-06 16:03:45 +0000

    move everything from config.php and ban.php into envvars

--- wwwroot/src/config.php
+++ wwwroot/src/config.php
@@ -3,20 +3,20 @@
 // Copy this file to "config.php" and change the following values
 
 // Password used to avoid captchas via the login page
-define("LOGIN_PASSWORD", "password"); 
+define("LOGIN_PASSWORD", getenv("LOGIN_PASSWORD")); 
 
 // Password for admins to do admin things via the login page
-define("ADMIN_PASSWORD", "password");
+define("ADMIN_PASSWORD", getenv("ADMIN_PASSWORD"));
 
 // Password used to ban users
-define("BAN_PASSWORD", "password");
+define("BAN_PASSWORD", getenv("BAN_PASSWORD"));
 
 // reCAPTCHA options
-define("RECAPTCHA_PUBLIC", "public api key");
-define("RECAPTCHA_PRIVATE", "private api key");
+define("RECAPTCHA_PUBLIC", getenv("RECAPTCHA_PUBLIC"));
+define("RECAPTCHA_PRIVATE", getenv("RECAPTCHA_PRIVATE"));
 
 // Secret regex to bypass captchas
-define("CAPTCHA_BYPASS", false);
+define("CAPTCHA_BYPASS", getenv("CAPTCHA_BYPASS"));
 
 // Leave this as is so the docker containers integrate correctly
 define("MYSQL_HOST", getenv("DB_HOSTNAME"));
