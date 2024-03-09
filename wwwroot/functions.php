a70c0628dc75411e58c23fc22d6df7d164b6cec6
Rachel Fish <itsrachelfish@gmail.com>
2024-03-09 03:39:18 -0700

    Change error reporting to hide notices / warnings

--- wwwroot/functions.php
+++ wwwroot/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-error_reporting(E_ALL ^ E_NOTICE);
+error_reporting(E_ERROR | E_PARSE);
 include("src/mysql.php");
 
 function Redirect($URL, $Time=2)
--- wwwroot/index.php
+++ wwwroot/index.php
@@ -1,6 +1,6 @@
 <?php
 
-error_reporting(E_ALL ^ E_NOTICE);
+error_reporting(E_ERROR | E_PARSE);
 session_start();
 
 require_once('src/config.php');
